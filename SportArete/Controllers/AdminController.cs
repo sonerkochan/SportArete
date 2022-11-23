7f9ad0f85b5c5c6317c4ebc2cbe25d9d101a56f9
Soner Kochan <sonibestbg@gmail.com>
2022-11-23 23:13:38 +0200

    Added: Administrator Menu where with option to add a product.

---
+++ SportArete/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportArete.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
